b3670c92f436043583beeb362be0e5ccc138f276
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-03-28 17:49:24 +0300

    Добавлен __all__

--- source/core/de/serving/datamart/modules/utils/__init__.py
+++ source/core/de/serving/datamart/modules/utils/__init__.py
@@ -6,3 +6,12 @@ from ._sql_executor import execute_sql_query
 from ._validate_data import is_cols_available
 from ._fetch_method import METHODS_MAP
 from ._merge_df_with_dm import merge_df_with_dm
+
+__ALL__: list[str] = [
+    "build_sql_query_by_date",
+    "build_sql_query_with_pre_agg_data",
+    "execute_sql_query",
+    "is_cols_available",
+    "merge_df_with_dm",
+    "METHODS_MAP",
+]
