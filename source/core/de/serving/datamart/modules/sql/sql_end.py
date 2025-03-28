87387258ba34f3176279da9eeee1dea4de91e029
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-03-28 14:08:59 +0300

    SQL-запросы к "Конечному остатку"

---
+++ source/core/de/serving/datamart/modules/sql/sql_end.py
@@ -0,0 +1,64 @@
+"""
+Модуль предоставляет методы формирования SQL-запросов к базе данных.
+"""
+
+# ##################################################
+# ИМПОРТЫ
+# ##################################################
+from typing import Optional, Tuple
+
+from loguru import logger
+
+from ..config.config import DEFAULT_VALID_AGG_FUNCS
+from ..utils._sql_builder import (
+    build_sql_query_by_date,
+    build_sql_query_with_pre_agg_data,
+)
+
+
+# ##################################################
+# КЛАССЫ
+# ##################################################
+class SQLEnd:
+    """
+    Класс предоставляет методы формирования SQL-запросов к базе данных.
+    """
+
+    # ##################################################
+    # API
+    # ##################################################
+    @staticmethod
+    def get_sql_query_final(
+        prefix: str,
+        doc_type: str,
+        period: int,
+        agg_cat_cols: Tuple[str, ...],
+        agg_dgt_cols: Tuple[str, ...],
+        agg_dt_cols: Tuple[str, ...],
+        agg_func: str,
+    ) -> Optional[str]:
+
+        if agg_func in ("first", "last"):
+            # Формирование SQL-запроса
+            query: str = build_sql_query_by_date(
+                prefix=prefix,
+                doc_type=doc_type,
+                agg_cat_cols=agg_cat_cols,
+                agg_dgt_cols=agg_dgt_cols,
+            )
+            logger.success("SQL-запрос успешно сформирован")
+            return query
+
+        elif agg_func in DEFAULT_VALID_AGG_FUNCS:
+            # Формирование SQL-запроса
+            query: str = build_sql_query_with_pre_agg_data(
+                prefix=prefix,
+                doc_type=doc_type,
+                period=period,
+                agg_cat_cols=agg_cat_cols,
+                agg_dgt_cols=agg_dgt_cols,
+                agg_dt_cols=agg_dt_cols,
+                agg_func=agg_func,
+            )
+            logger.success("SQL-запрос успешно сформирован")
+            return query
