6aa31f794d5a6fef790542a061a10f13b766c04d
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-03-28 14:09:22 +0300

    Форматирование

--- source/core/de/serving/datamart/modules/utils/__init__.py
+++ source/core/de/serving/datamart/modules/utils/__init__.py
@@ -3,11 +3,6 @@ from ._sql_builder import (
     build_sql_query_with_pre_agg_data
 )
 from ._sql_executor import execute_sql_query
-from ._merge_df_with_dm import merge_df_with_dm
-
-
-
-
-
 from ._validate_data import is_cols_available
 from ._fetch_method import METHODS_MAP
+from ._merge_df_with_dm import merge_df_with_dm
