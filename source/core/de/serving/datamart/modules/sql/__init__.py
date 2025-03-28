c03b349d649e179237d79d63216312c53023bbf9
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-03-28 17:49:15 +0300

    Обновлены импорты

--- source/core/de/serving/datamart/modules/sql/__init__.py
+++ source/core/de/serving/datamart/modules/sql/__init__.py
@@ -2,4 +2,5 @@ from .sql_start import SQLStart
 from .sql_income import SQLIncome
 from .sql_expend import SQLExpend
 from .sql_end import SQLEnd
+from .sql_transfer import SQLTransfer
 from .sql_arrival import SQLArrival
