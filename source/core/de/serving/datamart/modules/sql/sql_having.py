13066a09cb8a719515a643a9fe4ac5ebc4d08e67
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-03-31 14:02:36 +0300

    SQL-запросы по наличию

---
+++ source/core/de/serving/datamart/modules/sql/sql_having.py
@@ -0,0 +1,48 @@
+"""
+Модуль предоставляет методы формирования SQL-запросов к базе данных.
+"""
+
+# ##################################################
+# ИМПОРТЫ
+# ##################################################
+from typing import Optional, Tuple
+
+from loguru import logger
+
+from ..utils._sql_builder import (
+    build_sql_query_with_distinct
+)
+
+
+# ##################################################
+# КЛАССЫ
+# ##################################################
+class SQLHaving:
+    """
+    Класс предоставляет методы формирования SQL-запросов к базе данных.
+    """
+
+    # ##################################################
+    # API
+    # ##################################################
+    @staticmethod
+    def get_sql_query_stock(
+        prefix: str,
+        doc_type: str,
+        period: int,
+        agg_cat_cols: Tuple[str, ...],
+        agg_dgt_cols: Tuple[str, ...],
+        agg_dt_cols: Tuple[str, ...],
+    ) -> Optional[str]:
+
+        # Формирование SQL-запроса
+        query: str = build_sql_query_with_distinct(
+            prefix=prefix,
+            doc_type=doc_type,
+            period=period,
+            agg_cat_cols=agg_cat_cols,
+            agg_dgt_cols=agg_dgt_cols,
+            agg_dt_cols=agg_dt_cols,
+        )
+        logger.success("SQL-запрос успешно сформирован")
+        return query
