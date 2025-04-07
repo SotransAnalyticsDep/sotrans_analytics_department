8f9b5f5e3450c514c37461f16b8bef1c036983f0
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-04-07 17:39:26 +0300

    Добавлены варианты формирования данных по "Приемка товара на складах"

--- source/core/de/serving/datamart/modules/sql/sql_arrival.py
+++ source/core/de/serving/datamart/modules/sql/sql_arrival.py
@@ -9,7 +9,11 @@ from typing import Optional, Tuple
 
 from loguru import logger
 
-from ..utils._sql_builder import build_sql_query_by_date
+from ..config.config import DEFAULT_VALID_AGG_FUNCS
+from ..utils._sql_builder import (
+    build_sql_query_by_date,
+    build_sql_query_with_pre_agg_data,
+)
 
 
 # ##################################################
@@ -34,13 +38,27 @@ class SQLArrival:
         agg_func: str,
     ) -> Optional[str]:
 
+        if agg_func in ("first", "last"):
+            # Формирование SQL-запроса
+            query: str = build_sql_query_by_date(
+                prefix=prefix,
+                doc_type=doc_type,
+                agg_cat_cols=agg_cat_cols,
+                agg_dgt_cols=agg_dgt_cols,
+            )
+            logger.success("SQL-запрос успешно сформирован")
+            return query
 
-        # Формирование SQL-запроса
-        query: str = build_sql_query_by_date(
-            prefix=prefix,
-            doc_type=doc_type,
-            agg_cat_cols=agg_cat_cols,
-            agg_dgt_cols=agg_dgt_cols,
-        )
-        logger.success("SQL-запрос успешно сформирован")
-        return query
+        elif agg_func in DEFAULT_VALID_AGG_FUNCS:
+            # Формирование SQL-запроса
+            query: str = build_sql_query_with_pre_agg_data(
+                prefix=prefix,
+                doc_type=doc_type,
+                period=period,
+                agg_cat_cols=agg_cat_cols,
+                agg_dgt_cols=agg_dgt_cols,
+                agg_dt_cols=agg_dt_cols,
+                agg_func=agg_func,
+            )
+            logger.success("SQL-запрос успешно сформирован")
+            return query
