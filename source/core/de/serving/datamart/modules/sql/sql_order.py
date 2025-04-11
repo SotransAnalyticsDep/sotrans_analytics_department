3bb1c7ff63a34bf2466194961a67a1587cfd22d0
Dmitry 'ts.kdv.raz0r' Kolganov <ts.kdv.raz0r@ya.ru>
2025-04-11 15:35:11 +0300

    Реализован интерфейс "Текущие заказы"

---
+++ source/core/de/serving/datamart/modules/sql/sql_order.py
@@ -0,0 +1,45 @@
+"""
+Модуль предоставляет методы формирования SQL-запросов к базе данных.
+"""
+
+# ##################################################
+# ИМПОРТЫ
+# ##################################################
+from typing import Optional, Tuple
+
+from loguru import logger
+
+from ..utils._sql_builder import (
+    build_sql_query_order,
+)
+
+
+# ##################################################
+# КЛАССЫ
+# ##################################################
+class SQLOrder:
+    """
+    Класс предоставляет методы формирования SQL-запросов к базе данных.
+    """
+
+    # ##################################################
+    # API
+    # ##################################################
+    @staticmethod
+    def get_sql_query_order(
+            prefix: str,
+            doc_type: str,
+            agg_cat_cols: Tuple[str]
+    ) -> Optional[str]:
+    
+        # Формирование SQL-запроса
+        query: str = build_sql_query_order(
+            prefix=prefix,
+            doc_type=doc_type,
+            agg_cat_cols=agg_cat_cols,
+        )
+        logger.success("SQL-запрос успешно сформирован")
+        
+        return query
+
+
